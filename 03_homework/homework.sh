d70d0ff41052a455a7e532369b1df31e281fa2f9
melihalan <melihalan92@gmail.com>
2024-04-24 17:40:20 -0400

    Update homework.sh
    Added Homework

--- 03_homework/homework.sh
+++ 03_homework/homework.sh
@@ -7,21 +7,58 @@
 # On your terminal, input all the commands you have used to create the following:
 
 # 1. How would you create 5 directories? Feel free to use any name for your directories.
+mkdir dir1 dir2 dir3 dir4 dir5
 
 # 2. How would you verify the creation of all 5 directories?
+ls -F
 
 # 3. In each directory, how would you create 5 .txt files and write "I love data" into each within the directories?
 
+touch dir1/data1.txt
+echo "I love data" >> dir1/data1.txt
+touch dir2/data2.txt
+echo "I love data" >> dir2/data2.txt
+touch dir3/data3.txt
+echo "I love data" >> dir3/data3.txt
+touch dir4/data4.txt
+echo "I love data" >> dir4/data4.txt
+touch dir5/data5.txt
+echo "I love data" >> dir5/data5.txt
+
 # 4. How would you verify the presence of all 5 files?
+
+cd dir1/
+ls -F
+cd ../dir2/
+ls -F
+cd ../dir3/
+ls -F
+cd ../dir4/
+ls -F
+cd ../dir5/
+ls -F
+cd ../
 
 # 5. How would you append to one of the existing files " and machine learning!"?
 
+echo " and machine learning!" >> dir1/data1.txt
+
 # 6. How would you verify that the text was indeed appended to the existing file?
+
+cd dir1/
+cat data1.txt
 
 # 7. How would you delete all files except for the one with the appended text?
 
+
 # 8. How would you navigate back to the parent directory containing all the directories?
+
+cd ..
 
 # 9. How would you remove each directory along with its contents?
 
+rm -rf *
+
 # 10. How would you verify that all directories and files have been deleted?
+
+ls
